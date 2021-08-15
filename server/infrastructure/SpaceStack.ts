631740a8c97c6e922c51f47be7fa4d8c1be609dd
Janine Lapiz <jglapiz@up.edu.ph>
2021-08-15 21:35:44 +0800

    Added hello world lambda function

--- server/infrastructure/SpaceStack.ts
+++ server/infrastructure/SpaceStack.ts
@@ -1,8 +1,20 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
 
 export class SpaceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
   }
 }
---
+++ server/services/hello/hello.js
@@ -0,0 +1,6 @@
+exports.main = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: 'Hello from Lambda!'
+    }
+}
