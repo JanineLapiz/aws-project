13143005f5e81c43c671e9a4b8c960fea1a30686
Janine Lapiz <jglapiz@up.edu.ph>
2021-08-15 21:47:24 +0800

    Added api gateway and integrated it with helloLambda

--- server/infrastructure/SpaceStack.ts
+++ server/infrastructure/SpaceStack.ts
@@ -6,8 +6,11 @@ import {
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
 
 export class SpaceStack extends cdk.Stack {
+  private api = new RestApi(this, "SpaceApi");
+
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
@@ -16,5 +19,10 @@ export class SpaceStack extends cdk.Stack {
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
     });
+
+    // Hello API Lambda integration:
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
