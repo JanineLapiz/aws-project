df92404c263481d95cdc3cd941d51ca1cc26873f
Janine Lapiz <jglapiz@up.edu.ph>
2021-08-20 22:47:43 +0800

    Added permission for s3 to use lambda

--- server/infrastructure/SpaceStack.ts
+++ server/infrastructure/SpaceStack.ts
@@ -9,6 +9,7 @@ import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
 import { GenericTable } from "./GenericTable";
 import { NodejsFunction } from "aws-cdk-lib/lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/lib/aws-iam";
 
 export class SpaceStack extends cdk.Stack {
   private api = new RestApi(this, "SpaceApi");
@@ -21,19 +22,18 @@ export class SpaceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-      handler: "hello.main",
-    });
-
     const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
       entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     });
 
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources("*");
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
     // Hello API Lambda integration:
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     const helloLambdaResource = this.api.root.addResource("hello");
     helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
--- server/services/node-lambda/hello.ts
+++ server/services/node-lambda/hello.ts
@@ -1,9 +1,12 @@
-import { v4 } from "uuid";
+import { S3 } from "aws-sdk";
+
+const s3Client = new S3();
 
 const handler = async (event: any, context: any) => {
+  const buckets = await s3Client.listBuckets().promise();
   return {
     statusCode: 200,
-    body: "Hello from Lambda!" + v4,
+    body: "Here are buckets:" + JSON.stringify(buckets.Buckets),
   };
 };
 
