26cfa94150ce22a7509c6b8376cbea8136ec54b7
Janine Lapiz <jglapiz@up.edu.ph>
2021-08-20 23:26:48 +0800

    Added test file for debugging

--- server/services/node-lambda/hello.ts
+++ server/services/node-lambda/hello.ts
@@ -4,6 +4,8 @@ const s3Client = new S3();
 
 const handler = async (event: any, context: any) => {
   const buckets = await s3Client.listBuckets().promise();
+  console.log("THIS IS AN EVENT");
+  console.log(event);
   return {
     statusCode: 200,
     body: "Here are buckets:" + JSON.stringify(buckets.Buckets),
